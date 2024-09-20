67c7ae8c33e7f0d9bbb6c42149eb8bef0fa4ed3b
Rukhsar Ahmed <rukhsarnaich0@gmail.com>
2024-09-20 11:57:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,111 @@
+// Chapter 17-20
+
+//Question 01
+
+// var emptyMultiArray = [[]];
+
+//Question 02
+
+// var matrix = [
+//   [0, 1, 2, 3],
+//   [1, 0, 1, 2],
+//   [2, 1, 0, 1]
+// ];
+
+//Question 03
+
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+//Question 04
+
+// var number = prompt("Enter your table number here")
+// var length = prompt("Enter your length for table here")
+
+// for (var i = 1; i <= length; i++) {
+//     console.log(number + " x " + i + " = " + (number * i));
+// }
+
+//Question 05
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+//Question 06
+
+// for (var i = 1; i <= 15; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// for (var i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// for (var i = 1; i <= 19; i += 2) {
+//   console.log(i);
+// }
+
+// for (var i = 2; i <= 20; i += 2) {
+//   console.log(i + "k");
+// }
+
+//Question 07
+
+// var A = ["cake", "apple" ,"pie", "cookie", "chips", "patties"];
+
+// var userCity = prompt("Enter the item name");
+
+// var flag = false;
+
+// for (var i = 0; i < A.length; i++) {
+//   if (A[i] === userCity) {
+//     flag = true;
+//     break;
+//   }
+// }
+
+// if (flag) {
+//   alert("Yes, " + userCity + " is found in the list.");
+// } else {
+//   alert("No, " + userCity + " is not found in the list.")
+// }
+
+//Question 08
+
+// console.log(A = [24, 53, 78, 91, 12]);
+// var largest = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+//   if (A[i] > largest) {
+//     largest = A[i];
+//   }
+// }
+
+// console.log("The largest number is " + largest);
+
+//Question 09
+
+// console.log(A = [24, 53, 78, 91, 12]);
+
+// var smallest = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+//   if (A[i] < smallest) {
+//     smallest = A[i];
+//   }
+// }
+
+// console.log("The smallest number is " + smallest);
+
+//Question 10
+
+// for (var i = 5; i <= 100; i += 5) {
+//   console.log(i);
+// }
